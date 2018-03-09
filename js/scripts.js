c0a1fc8d71da9f50f51473b931b9a0e4584354b7
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-09 06:58:09 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// scripts.js
+
+$(document).ready(function(){
+	console.log('DOM loaded - you can have fun');
+});
+
+var span = $("span");
+    span.each(function(index, element) {
+	if(index % 2 == 0) {
+		$(element).css('color', 'red');
+	};
+        console.log(element);
+});
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+    
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button);
+    
+});
+
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
+
